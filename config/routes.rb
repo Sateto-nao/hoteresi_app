4ca02a1930753db7f125d67d7515d7cb93d3a8a4
Naonao <fn405432@gmail.com>
2021-04-23 13:36:54 +0900

    update_users_show_edit

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
   resources :users, :only => [:show, :edit]
+
 end
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
