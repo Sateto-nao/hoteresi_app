5cdfcd3fa765d323b7b08c1f46e068d13867f806
Naonao <fn405432@gmail.com>
2021-05-04 10:39:45 +0900

    add_mysql.rb

--- db/migrate/20210503235942_remove_postcode_from_rooms.rb
+++
@@ -1,9 +0,0 @@
-class RemovePostcodeFromRooms < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :rooms, :prefecture_code, :integer
-    remove_column :rooms, :postcode, :integer
-    remove_column :rooms, :address_city, :string
-    remove_column :rooms, :address_street, :string
-    remove_column :rooms, :address_building, :string
-  end
-end
