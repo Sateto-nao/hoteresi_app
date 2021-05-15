f5edbc4d73ebe0bed86bd3d06997c4ef04a5f4ba
Naonao <fn405432@gmail.com>
2021-05-16 08:48:40 +0900

    delete_Mypage::Myhotels_controller

--- app/controllers/users/myhotels_controller.rb
+++
@@ -1,8 +0,0 @@
-class Mypage::MyhotelsController < ApplicationController
-
-  def index
-    @user = User.find(params[:id])
-    @hotels = Hotel.all.includes(:users)
-    @hotels = Hotel.joins(:user).find(params[:id])
-  end
-end
