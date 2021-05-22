4b00a8c7ec798b17e104fd87fc8691278ba606c6
Naonao <fn405432@gmail.com>
2021-05-22 16:24:44 +0900

    fix_migration

--- db/migrate/20210505010602_chage_date_to_favorite.rb
+++
@@ -1,6 +0,0 @@
-class ChageDateToFavorite < ActiveRecord::Migration[6.1]
-  def change
-     remove_column :favorites, :user_id, :bigint
-     remove_column :favorites, :room_id, :bigint
-  end
-end
