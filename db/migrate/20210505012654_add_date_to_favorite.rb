734e0d69602057b50bed7838e74fd1d28ad8b59d
Naonao <fn405432@gmail.com>
2021-05-22 17:04:33 +0900

    update_migration_file

--- db/migrate/20210505012654_add_date_to_favorite.rb
+++
@@ -1,6 +0,0 @@
-class AddDateToFavorite < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :favorites, :user, foreign_key: true
-    add_reference :favorites, :room, foreign_key: true
-  end
-end
